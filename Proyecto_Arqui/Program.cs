625fa5cc611aa3b783c866edae4483768807d198
danielperaltamadriz <danielperaltamadriz@gmail.com>
2016-10-19 14:05:16 -0600

    cambios en cache

--- Proyecto_Arqui/Program.cs
+++ Proyecto_Arqui/Program.cs
@@ -12,27 +12,40 @@ namespace Proyecto_Arqui
         static int[,] mat_contextos;            //matriz de contextos
         double ciclos_reloj;                    //cantidad de ciclos de reloj
         static int quantum_total;               //valor del usuario para quantum
-        
-        [ThreadStatic] static int quantum;          //cantidad de instrucciones ejecutadas
-        [ThreadStatic] static int[,] cache_datos;   //matriz de cache de datos
-        [ThreadStatic] static int[,] cache_instruc; //matriz de cache de instrucciones
-        [ThreadStatic] static int[] registros;      //registros propios del nucleo
-        [ThreadStatic] static int PC;               //la siguiente instruccion a ejecutar
+
+        [ThreadStatic]
+        static int quantum;          //cantidad de instrucciones ejecutadas
+
+        static int[,] cache_datos1;   //matriz de cache de datos
+        static int[,] cache_datos2;   //matriz de cache de datos
+        static int[,] cache_datos3;   //matriz de cache de datos
+        static int[,] cache_instruc1; //matriz de cache de instrucciones
+        static int[,] cache_instruc2; //matriz de cache de instrucciones
+        static int[,] cache_instruc3; //matriz de cache de instrucciones
+        [ThreadStatic]
+        static int[] registros;      //registros propios del nucleo
+        [ThreadStatic]
+        static int PC;               //la siguiente instruccion a ejecutar
+        static bool lento;
 
         static List<int> hilillos_tomados;
         int ultimo_mem_inst;            //'puntero' a ultimo lleno en memoria de instrucciones
         static int cant_hilillos;       //cantidad de hilillos definida por el usuario
         string file_path;               //direccion del directorio donde estaran los hilillos
+        private readonly DireccionarMemoria _direccionarMemoria;
+        private readonly DireccionarEstructuraDeDatos _direccionarDatos;
 
         /*Metodos direccionamiento de memoria--------------------------------*/
         private int dir_a_bloque(int direccion)
         {
             return direccion / 16;
         }
+
         private int dir_a_palabra(int direccion)
         {
             return (direccion % 16) / 4;
         }
+
         private int bloque_a_cache(int bloque)
         {
             return bloque % 4;
@@ -43,28 +56,30 @@ namespace Proyecto_Arqui
         {
             return direccion / 4;
         }
+
         private int vectorDatos_a_direccion(int indiceVector)
         {
             return indiceVector * 4;
         }
+
         private int direccion_a_vectorInstrucciones(int direccion)
         {
             return direccion - 384;
         }
+
         private int vectorInstrucciones_a_direccion(int indiceVector)
         {
             return indiceVector + 384;
         }
-
-        
 
         /*----------------------------------------------------------------------------------------------*/
         /*Inicio del programa-----------------------------------------------*/
         public Program() //constructor, se inicializan las variables
         {
             mem_principal_datos = new int[96];
-            for (int i=0; i<=95; i++) {
-                mem_principal_datos[i]= 1;
+            for (int i = 0; i <= 95; i++)
+            {
+                mem_principal_datos[i] = 1;
             }
 
             mem_principal_instruc = new int[640];
@@ -72,7 +87,7 @@ namespace Proyecto_Arqui
             {
                 mem_principal_instruc[j] = 1;
             }
-			ultimo_mem_inst = 0;
+            ultimo_mem_inst = 0;
             ciclos_reloj = 0;
             cant_hilillos = 0;
             quantum_total = 0;
@@ -93,49 +108,58 @@ namespace Proyecto_Arqui
 
         /*Lectura txts------------------------------------------------------*/
         //Metodo auxiliar que sirve para leer un solo hilillo y meterlo en memoria
-        private void leer_hilillo_txt(int i) 
+        private void leer_hilillo_txt(int i)
         {
-			char[] delimiterChars = { ' ', '\n'};
-			string text = System.IO.File.ReadAllText(@file_path + i.ToString() + ".txt");
-			string[] words = text.Split(delimiterChars);
-			int ultimo_viejo = ultimo_mem_inst;
+            char[] delimiterChars = { ' ', '\n' };
+            string text = System.IO.File.ReadAllText(@file_path + i.ToString() + ".txt");
+            string[] words = text.Split(delimiterChars);
+            int ultimo_viejo = ultimo_mem_inst;
 
-			foreach (string s in words)
-			{
-				mem_principal_instruc[ultimo_mem_inst++]= Int32.Parse(s);
-			}
-            mat_contextos[i-1, 32] = ultimo_viejo;   //el PC
+            foreach (string s in words)
+            {
+                mem_principal_instruc[ultimo_mem_inst++] = Int32.Parse(s);
+            }
+            mat_contextos[i - 1, 32] = ultimo_viejo;   //el PC
 
         }
-		public void leer_muchos_hilillos() { //permite cargar todos los hilillos desde el txt a memoria
+        public void leer_muchos_hilillos()
+        { //permite cargar todos los hilillos desde el txt a memoria
             mat_contextos = new int[cant_hilillos, 34];
-            for (int i = 1; i <= cant_hilillos; i++) {
-				leer_hilillo_txt(i);
-			}
-            mem_principal_instruc[ultimo_mem_inst]=1;
+            for (int i = 1; i <= cant_hilillos; i++)
+            {
+                leer_hilillo_txt(i);
+            }
+            mem_principal_instruc[ultimo_mem_inst] = 1;
 
 
-            for (int i = 0; i < cant_hilillos; i++){
-                for (int j = 0; j < 34; j++){
-                    if (j != 32) {
+            for (int i = 0; i < cant_hilillos; i++)
+            {
+                for (int j = 0; j < 34; j++)
+                {
+                    if (j != 32)
+                    {
                         mat_contextos[i, j] = 0;
                     }
                 }
             }
         }
 
-        
+
         /*Hilos----------------------------------------------------------*/
         static void escogerHilillo()
         {
             bool lockWasTaken = false;
             var temp = mem_principal_instruc;
-            try{
+            try
+            {
                 Monitor.Enter(temp, ref lockWasTaken);
                 bool hilillo_escogido = false;
-                for (int i=0; i< cant_hilillos && hilillo_escogido==false; i++) {
-                    if (mat_contextos[i, 33] != 1) {
-                        if (!hilillos_tomados.Contains(i + 1)) {
+                for (int i = 0; i < cant_hilillos && hilillo_escogido == false; i++)
+                {
+                    if (mat_contextos[i, 33] != 1)
+                    {
+                        if (!hilillos_tomados.Contains(i + 1))
+                        {
                             hilillos_tomados.Add(i + 1);  //poner numero de hilillo, correspondiente con el PC
                             PC = mat_contextos[i, 32];
                             hilillo_escogido = true;
@@ -144,30 +168,30 @@ namespace Proyecto_Arqui
                     }
                 }
             }
-            finally{
-                if (lockWasTaken){
+            finally
+            {
+                if (lockWasTaken)
+                {
                     Monitor.Exit(temp);
                 }
             }
         }
         private static void procesoDelNucelo()
         {
-            //INICIALIZACION
-            cache_datos = new int[6,4];
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 4; j++) {
-                    cache_datos[i, j] = 0;
-
-                }
-            }
-            cache_instruc = new int[5, 16];
-            for (int i = 0; i < 5; i++)
+            switch (System.Threading.Thread.CurrentThread.Name)
             {
-                for (int j = 0; j < 16; j++)
-                {
-                    cache_instruc[i, j] = 0;
-
-                }
+                case "Hilo1":
+                    inicializarCacheDatos(ref cache_datos1);
+                    inicializarCacheInstrucciones(ref cache_instruc1);
+                    break;
+                case "Hilo2":
+                    inicializarCacheDatos(ref cache_datos2);
+                    inicializarCacheInstrucciones(ref cache_instruc2);
+                    break;
+                case "Hilo3":
+                    inicializarCacheDatos(ref cache_datos3);
+                    inicializarCacheInstrucciones(ref cache_instruc3);
+                    break;
             }
             registros = new int[34];
             quantum = 0;
@@ -186,13 +210,70 @@ namespace Proyecto_Arqui
             //Tomar en cuenta el quantum local
         }
 
+        private static void inicializarCacheDatos(ref int[,] cache)
+        {
+            cache = new int[6, 4];
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    cache[i, j] = 0;
+
+                }
+            }
+        }
+
+        private static void inicializarCacheInstrucciones(ref int[,] cache)
+        {
+            cache = new int[5, 16];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    cache[i, j] = 0;
+
+                }
+            }
+        }
+
+
+
+        private static void modoDeEjejcucion()
+        {
+            bool failed = true;
+            while (failed)
+            {
+                Console.WriteLine("\nDesea que el modo de ejecución sea Lento(1) o Rápido(2)");
+                int parsed;
+                if (Int32.TryParse(Console.ReadLine(), out parsed))
+                {
+                    switch (parsed)
+                    {
+                        case 1:
+                            lento = true;
+                            failed = false;
+                            break;
+                        case 2:
+                            lento = false;
+                            failed = false;
+                            break;
+                        default:
+                            Console.WriteLine("\nRespuesta en el formato incorrecto, sólo puede contestar 1 para Lento o 2 para Rápido");
+                            break;
+                    }
+                }
+            }
+
+        }
+
         /*-------------------------------------------------------------------*/
         /*MAIN---------------------------------------------------------------*/
         static void Main(string[] args)
         {
             Program p = new Program();
             p.menu_usuario();
-			p.leer_muchos_hilillos();
+            p.leer_muchos_hilillos();
+            modoDeEjejcucion();
             /*//IMPRESION DE MEMORIA INSTRUCCIONES
             for (int i = 0; i < mem_principal_instruc.Length; i++) {
                 if (mem_principal_instruc[i] != 1) {
@@ -207,7 +288,7 @@ namespace Proyecto_Arqui
             {
                 Console.Write(mat_contextos[i, 32]+" ");
             }*/
-            
+
             //crear nucleos
             Thread myThread;
             for (int i = 0; i < 3; i++)
